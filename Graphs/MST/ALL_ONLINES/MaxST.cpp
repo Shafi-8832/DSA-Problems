0cf45f2efeb0c95648ed371317f536acae66b55e
Ahnaf Shafi <shafi.ahnaf3412@gmail.com>
2026-07-03 22:31:35 +0600

    Rest

---
+++ Graphs/MST/ALL_ONLINES/MaxST.cpp
@@ -0,0 +1,125 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+using pll = pair<ll, ll>;
+#define f(t, i, x, y) for (t (i)=(x); (i)<(y); (i)++)
+#define fe(t, i, x, y) for (t (i)=(x); (i)<=(y); (i)++)
+
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define intmin INT64_MIN
+#define int long long
+
+class DSU {
+    public:
+    vector<int> parent;
+    vector<int> group_size;
+    int connected_components;
+    int max_size = 1;
+
+
+    DSU(int n) {
+        parent.resize(n + 1);
+        group_size.resize(n + 1, 1);
+        connected_components = n; // each vertex is a stand-alone component itself.
+
+        for (int i=0; i<=n; i++) parent[i] = i; // each vertex is a parent to itself.
+        // iota(parent.begin(), parent.end(), 0); same as parent[i] = i
+    }
+
+    int find(int i) {
+        if (parent[i] == i) return i;
+        return parent[i] = find(parent[i]); // path compression in one line for amortized O(1) TC
+    }
+
+    bool unite(int i, int j) {
+        int parent_i = find(i);
+        int parent_j = find(j);
+
+        if (parent_i == parent_j) return false; // didn't change the component_count, cause same tree
+
+        if (group_size[parent_i] > group_size[parent_j]) {
+            parent[parent_j] = parent_i;
+            group_size[parent_i] += group_size[parent_j];
+            max_size = max(max_size, group_size[parent_i]);
+        }
+        else {
+            parent[parent_i] = parent_j;
+            group_size[parent_j] += group_size[parent_i];
+            max_size = max(max_size, group_size[parent_j]);
+        }
+
+        // successfully merged two groups == edge created between two forests so,,,
+        connected_components--; 
+
+        return true;
+    }
+
+    bool connected(int a, int b) {return find(a) == find(b); }
+    int size(int a) { return group_size[find(a)]; }
+
+    // IMPORTANT : when checking group_size or anything about DSU, always ask the parent, leader of the of the group.
+    // NEVER ASK THE INDIVIDUAL node, they hold outdated values
+
+};
+
+typedef struct edge {
+    int u, v, w;
+    bool operator<(const edge& other) {
+        return w < other.w;
+    }
+} edge;
+
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);    
+    
+    int n, m; cin >> n >> m;
+    vector<tiii> edges;
+    for (int i=0; i<m; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        edges.pb({-c, a, b});
+    }
+
+    sort(all(edges));
+
+
+    int mst = 1;
+    int edges_taken = 0;
+    bool possible = false;
+
+
+    DSU dsu(n);
+    for (auto& e : edges) {
+        int w = get<0>(e);
+        int a = get<1>(e);
+        int b = get<2>(e);
+
+        if (dsu.unite(a, b)) {
+            edges_taken++;
+            mst *= w;
+
+            if (edges_taken == n-1) {
+                possible = true;
+                break;
+            }
+        }
+    }
+
+    if (!possible) {
+        cout << -1 << '\n';
+        return 0;
+    }
+
+    cout << mst << '\n';
+
+    return 0;
+}
---
+++ Graphs/MST/ALL_ONLINES/batch21AReverseDelMST.cpp
@@ -0,0 +1,158 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+using pll = pair<ll, ll>;
+#define f(t, i, x, y) for (t (i)=(x); (i)<(y); (i)++)
+#define fe(t, i, x, y) for (t (i)=(x); (i)<=(y); (i)++)
+
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define intmin INT64_MIN
+#define int long long
+
+
+class DSU {
+    public:
+    vector<int> parent;
+    vector<int> group_size;
+    int connected_components;
+    int max_size = 1;
+
+
+    DSU(int n) {
+        parent.resize(n + 1);
+        group_size.resize(n + 1, 1);
+        connected_components = n; // each vertex is a stand-alone component itself.
+
+        for (int i=0; i<=n; i++) parent[i] = i; // each vertex is a parent to itself.
+        // iota(parent.begin(), parent.end(), 0); same as parent[i] = i
+    }
+
+    int find(int i) {
+        if (parent[i] == i) return i;
+        return parent[i] = find(parent[i]); // path compression in one line for amortized O(1) TC
+    }
+
+    bool unite(int i, int j) {
+        int parent_i = find(i);
+        int parent_j = find(j);
+
+        if (parent_i == parent_j) return false; // didn't change the component_count, cause same tree
+
+        if (group_size[parent_i] > group_size[parent_j]) {
+            parent[parent_j] = parent_i;
+            group_size[parent_i] += group_size[parent_j];
+            max_size = max(max_size, group_size[parent_i]);
+        }
+        else {
+            parent[parent_i] = parent_j;
+            group_size[parent_j] += group_size[parent_i];
+            max_size = max(max_size, group_size[parent_j]);
+        }
+
+        // successfully merged two groups == edge created between two forests so,,,
+        connected_components--; 
+
+        return true;
+    }
+
+    bool connected(int a, int b) {return find(a) == find(b); }
+    int size(int a) { return group_size[find(a)]; }
+
+    // IMPORTANT : when checking group_size or anything about DSU, always ask the parent, leader of the of the group.
+    // NEVER ASK THE INDIVIDUAL node, they hold outdated values
+
+};
+
+typedef struct edge {
+    int u, v, w;
+    bool operator<(const edge& other) {
+        return w < other.w;
+    }
+} edge;
+
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);    
+    
+    // does deleting an edge e, disconnect the graph?? ---> not easy to answer, need advanced data structures;
+
+    int n, m; cin >> n >> m;
+
+    vector<set<int>> adj(n, set<int>());
+
+    vector<tiii> edges;
+
+    int mst = 0;
+    for (int i=0; i<m; i++) {
+        int u, v, w; cin >> u >> v >> w;
+        
+        adj[u].insert(v);
+        adj[v].insert(u);
+
+        edges.pb({w, u, v});
+        mst += w;
+    }
+
+    sort(all(edges), greater<>()); // DESC
+
+    int edges_taken = m;
+
+    for (auto& e : edges) {
+        int w = get<0>(e);
+        int u = get<1>(e);
+        int v = get<2>(e);
+
+        bool doesnt_hurt = false;
+
+        // delete
+        adj[u].erase(v);
+        adj[v].erase(u);
+
+        bitset<1000> visited;
+        queue<int> q;
+        visited[u] = 1;
+        q.push(u);
+        while (!q.empty()) {
+            int U = q.front(); q.pop();
+            if (U == v) {
+                doesnt_hurt = true;
+                break; // end bfs
+            }
+            for (int v : adj[U]) {
+                if (!visited[v]) {q.push(v); visited[v]=1;}
+            }
+        }
+
+        // million dollar question : 
+        if (!doesnt_hurt) { // put the edge back, we need the essential edge for later BFSs
+            adj[u].insert(v);
+            adj[v].insert(u);
+        }
+        else if (doesnt_hurt)) {
+            mst -= w;
+            edges_taken--;
+
+            if (edges_taken == n - 1) {
+            possible = true;
+            break;
+            }
+        }
+    }
+
+    if (!possible) {
+        cout << -1 << '\n';
+        return 0;
+    }
+    else cout << mst << '\n';
+
+    return 0;
+}
