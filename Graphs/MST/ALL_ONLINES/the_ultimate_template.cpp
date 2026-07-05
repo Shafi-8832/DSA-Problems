a344b6477b9b0ff478e3ab8979dbf9e7c50170d2
Ahnaf Shafi <shafi.ahnaf3412@gmail.com>
2026-07-05 13:56:47 +0600

    allah vorosha

--- Graphs/MST/ALL_ONLINES/the_ultimate_template.cpp
+++ Graphs/MST/ALL_ONLINES/the_ultimate_template.cpp
@@ -17,21 +17,31 @@ using pll = pair<ll, ll>;
 #define intmin INT64_MIN
 #define int long long
 
+
 class DSU {
     public:
     vector<int> parent;
     vector<int> group_size;
+    vector<int> safe_count;
     int connected_components;
     int max_size = 1;
+    int largest_leader = 1;
 
 
     DSU(int n) {
         parent.resize(n + 1);
         group_size.resize(n + 1, 1);
+        safe_count.resize(n + 1, 0);
+
         connected_components = n; // each vertex is a stand-alone component itself.
 
         for (int i=0; i<=n; i++) parent[i] = i; // each vertex is a parent to itself.
         // iota(parent.begin(), parent.end(), 0); same as parent[i] = i
+
+        // count safe nodes here
+        for (int i=0; i<=n; i++) {
+            if (/*condition to be safe node*/) safe_count[i] = 1;
+        }
     }
 
     int find(int i) {
@@ -48,12 +58,23 @@ class DSU {
         if (group_size[parent_i] > group_size[parent_j]) {
             parent[parent_j] = parent_i;
             group_size[parent_i] += group_size[parent_j];
-            max_size = max(max_size, group_size[parent_i]);
+            safe_count[parent_i] += safe_count[parent_j];
+
+            if (group_size[parent_i] > max_size) {
+                max_size = group_size[parent_i];
+                largest_leader = parent_i;
+            }
         }
         else {
             parent[parent_i] = parent_j;
             group_size[parent_j] += group_size[parent_i];
-            max_size = max(max_size, group_size[parent_j]);
+            safe_count[parent_j] += safe_count[parent_i];
+
+
+            if (group_size[parent_i] > max_size) {
+                max_size = group_size[parent_i];
+                largest_leader = parent_i;
+            }
         }
 
         // successfully merged two groups == edge created between two forests so,,,
@@ -76,6 +97,34 @@ typedef struct edge {
         return w < other.w;
     }
 } edge;
+
+class Edge {
+    public: 
+    int u, v;
+    int original_weight;
+    int risky_points;
+    int effective_weight;
+
+    Edge(int U, int V, int org, int rp, int P) {
+        u = U;
+        v = V;
+        original_weight = org;
+        risky_points = rp;
+
+        effective_weight = original_weight + P * risky_points;
+    }
+
+    bool operator<(const Edge& other) {
+        bool this_safe = (risky_points == 0);
+        bool other_safe = (other.risky_points == 0);
+
+        if (this_safe != other_safe) {
+            return this_safe > other_safe;
+        }
+
+        return effective_weight < other.effective_weight;
+    }
+};
 
 
 
