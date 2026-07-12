8e1a43f6c5f523052639fe00c5c421d3e55e53b5
Ahnaf Shafi <shafi.ahnaf3412@gmail.com>
2026-07-12 15:34:28 +0600

    template fix 3

--- Graphs/Single Source Shortest Path/ALL ONLINES/batch17SecB.cpp
+++ Graphs/Single Source Shortest Path/ALL ONLINES/batch17SecB.cpp
@@ -25,24 +25,30 @@ int32_t main() {
     int n, e; cin >> n >> e;
 
     vector<vector<pii>> adjList(n + 1, vector<pii>());
+    vector<int> d(n + 1, INF);
+
+    vector<int> vertex(n + 1);
+    for (int i=1; i<=n; i++) {
+        cin >> vertex[i];
+    }
 
     for (int i=0; i<e; i++) {
         int u, v;
         int w;
-
+        
         cin >> u >> v >> w;
+        w = w + vertex[v];
 
-        // w = 
+
         adjList[u].pb({w, v});
     }
 
-    vector<int> d(n + 1, INF);
 
     // Shortest Path reconstruct
     vector<int> parent(n + 1, -1);
 
-    int source; cin >> source;
-    d[source] = 0;
+    int source, dest; cin >> source >> dest;
+    d[source] = vertex[source]; // *****
 
     set<pii> pq; // {d value, destination_node}
 
@@ -64,13 +70,19 @@ int32_t main() {
         }
     }
 
-    vector<int> shortest_path;
-    int dest; cin >> dest;
+    cout << "Shortest path cost : " << d[dest];
 
+    vector<int> shortest_path;
     for (int cur = dest; cur != -1; cur = parent[cur]) {
         shortest_path.pb(cur);
     }
     reverse(all(shortest_path));
+
+    for (auto& v : shortest_path) {
+        cout << v;
+        if (v != dest) cout << " -> ";
+    }
+
     
     return 0;
 }
